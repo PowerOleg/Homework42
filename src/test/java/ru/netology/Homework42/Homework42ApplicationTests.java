0ffe47cc648c3e6f98ca77172859448c2624a12e
PowerOleg <geraskin.oleg@mail.ru>
2023-04-18 00:05:07 +0500

    Initial commit

---
+++ src/test/java/ru/netology/Homework42/Homework42ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.Homework42;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework42ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
